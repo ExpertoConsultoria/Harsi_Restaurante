9d46f7be8a7539ea415ffa45184d4b6f67e85d28
Victor Gabriel Valencia Garcia <110759131+VictorGabrielValenciaGarcia@users.noreply.github.com>
2024-02-07 11:08:44 -0600

    Corrección del redireccionamiento del Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Hotel</title>
+        <title>Restaurante</title>
 
-        <!-- Fonts -->
+        <!-- Fonts <-->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
  <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
@@ -15,19 +16,28 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+    <link rel="icon" href="../imagenes-05.ico" type="image/x-icon  ">
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+background: #ECE9E6;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #FFFFFF, #ECE9E6);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #FFFFFF, #ECE9E6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
             }
+
+            @font-face {
+    font-family: Montserrat;
+    src: url('/fonts/Montserrat-SemiBold.ttf');
+    }
 
             .full-height {
                 height: 100vh;
@@ -70,29 +80,54 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            .btn-primary{
+            color: white;
+            background-color: #006b8e;
+            border: #006b8e;
+            border-radius: 10px;
+             font-family: Montserrat;
+    src: url('/fonts/Montserrat-SemiBold.ttf');
+    font-family: 'Montserrat';
+            }
+            .btn-primary:hover{
+            color: white;
+            background-color: #006b8e;
+            border: #006b8e;
+            border-radius: 10px;
+           font-family: Montserrat;
+    src: url('/fonts/Montserrat-SemiBold.ttf');
+    font-family: 'Montserrat';
+
+            }
+
+
         </style>
+
+
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Inicio</a>
-                    @else
-                        {{-- <a href="{{ route('login') }}">Acceder</a> --}}
 
-                      @if (Route::has('register'))
-                            {{-- <a href="{{ route('register') }}">Registrar</a> --}}
-                        @endif
-                    @endauth
-                </div>
-            @endif
-<div class="container-fluid">
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/inicio') }}">Inicio</a>
+            @else
+
+
+              @if (Route::has('register'))
+
+                @endif
+            @endauth
+        </div>
+    @endif
+
+    <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-header" style="Background-color:transparent">
-                     <img src="../../img/hotelink.png" class="rounded mx-auto d-block" alt="..." width="300" height="100">
+                     <img src="../img/imagenes-07.png" class="rounded mx-auto d-block" alt="..." width="300" height="100">
                     </div>
 
                     <div class="card-body">
@@ -125,7 +160,7 @@
                             <div class="form-group">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Iniciar Session') }}
+                                        {{ __('Iniciar Sesión') }}
                                     </button>
                                 </div>
                             </div>
@@ -137,5 +172,7 @@
     </div>
 
 </div>
-    </body>
+
+
+</body>
 </html>
