39bf410bee035d64b7a9adfa2b807022ada26742
Victor Gabriel Valencia Garcia <110759131+VictorGabrielValenciaGarcia@users.noreply.github.com>
2024-10-08 11:00:19 -0600

    REFACTOR: Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,19 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Restaurante</title>
 
-        <!-- Fonts <-->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Restaurante</title>
 
- <!-- Scripts -->
+    {{-- Icono --}}
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/fav1.png') }}">
+
+    <!-- Fonts <-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
@@ -20,159 +24,173 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="icon" href="../imagenes-05.ico" type="image/x-icon  ">
-        <!-- Styles -->
-        <style>
-            html, body {
-background: #ECE9E6;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to bottom, #FFFFFF, #ECE9E6);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to bottom, #FFFFFF, #ECE9E6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #ece9e6;
+            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23ffffff' fill-opacity='0.54' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
 
 
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            @font-face {
-    font-family: Montserrat;
-    src: url('/fonts/Montserrat-SemiBold.ttf');
-    }
+        @font-face {
+            font-family: Montserrat;
+            src: url('/fonts/Montserrat-SemiBold.ttf');
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .card {
+            border-radius: 1rem;
+            border-color: #5a5c5d22
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            .btn-primary{
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+        .btn-primary {
             color: white;
             background-color: #006b8e;
             border: #006b8e;
             border-radius: 10px;
-             font-family: Montserrat;
-    src: url('/fonts/Montserrat-SemiBold.ttf');
-    font-family: 'Montserrat';
-            }
-            .btn-primary:hover{
+            font-family: Montserrat;
+            src: url('/fonts/Montserrat-SemiBold.ttf');
+            font-family: 'Montserrat';
+        }
+
+        .btn-primary:hover {
             color: white;
             background-color: #006b8e;
             border: #006b8e;
             border-radius: 10px;
-           font-family: Montserrat;
-    src: url('/fonts/Montserrat-SemiBold.ttf');
-    font-family: 'Montserrat';
+            font-family: Montserrat;
+            src: url('/fonts/Montserrat-SemiBold.ttf');
+            font-family: 'Montserrat';
 
-            }
+        }
+
+    </style>
 
 
-        </style>
+</head>
 
+<body>
 
-    </head>
-    <body>
-
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
         <div class="top-right links">
             @auth
-                <a href="{{ url('/inicio') }}">Inicio</a>
+            <a href="{{ url('/inicio') }}">Inicio</a>
             @else
 
 
-              @if (Route::has('register'))
+            @if (Route::has('register'))
 
-                @endif
+            @endif
             @endauth
         </div>
-    @endif
+        @endif
 
-    <div class="container-fluid">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header" style="Background-color:transparent">
-                     <img src="../img/imagenes-07.png" class="rounded mx-auto d-block" alt="..." width="300" height="100">
-                    </div>
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header" style="Background-color:transparent">
+                            <img src="../img/imagenes-07.png" class="mx-auto rounded d-block" alt="..." width="300"
+                                height="100">
+                        </div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                             <div class="input-group mb-2">
-                                <div class="input-group-prepend">
-                                  <div class="input-group-text">@</div>
-                                </div>
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus placeholder="Usuario">
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="mb-2 input-group">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text">@</div>
+                                    </div>
+                                    <input id="email" type="email"
+                                        class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                        name="email" value="{{ old('email') }}" required autofocus
+                                        placeholder="Usuario">
                                     @if ($errors->has('email'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
                                     @endif
-                              </div>
-                              <br>
-                            <div class="input-group mb-2">
-                                <div class="input-group-prepend">
-                                  <div class="input-group-text">**</div>
                                 </div>
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="Contraseña">
+                                <br>
+                                <div class="mb-2 input-group">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text">**</div>
+                                    </div>
+                                    <input id="password" type="password"
+                                        class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                        name="password" required placeholder="Contraseña">
                                     @if ($errors->has('password'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
                                     @endif
-                            </div>
-                              <br>
-                            <div class="form-group">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Iniciar Sesión') }}
-                                    </button>
                                 </div>
-                            </div>
-                        </form>
+                                <br>
+                                <div class="row justify-content-center">
+                                    <div class="col-md-8 offset-3">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Iniciar Sesión') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-</div>
+    </div>
 
 
 </body>
+
 </html>
