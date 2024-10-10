dd0ef47d5c65244cdfb9252b0032b5ee8b8914cb
Victor Gabriel Valencia Garcia <110759131+VictorGabrielValenciaGarcia@users.noreply.github.com>
2024-10-10 11:10:30 -0600

    ADD: Ticket Interno y Externo

--- resources/views/Ordenes/show.blade.php
+++ resources/views/Ordenes/show.blade.php
@@ -10,12 +10,12 @@
     <title>Ticket de Venta | {{ $orden->id }}</title>
 
     {{-- Boostrap --}}
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
     {{-- Jquery --}}
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 
     {{-- My Styles --}}
     <style>
@@ -47,6 +47,30 @@
             text-align: center;
         }
 
+        /* Nav - Tabs */
+        .nav-tabs {
+            color: #0d6889;
+        }
+
+        .nav-item:hover {
+            filter: brightness(0.95); /* Disminuye el brillo para oscurecer */
+        }
+
+        .nav-tabs .nav-item.show .nav-link,
+        .nav-tabs .nav-link.active {
+            color: #ffffff;
+            background-color: #0d6889;
+            border-color: #dee2e6 #dee2e6 #dee2e6 #dee2e6;
+        }
+
+        .nav-tabs .nav-item.show .nav-link,
+        .nav-tabs .nav-link {
+            border: 1px solid #dee2e6;
+            color: #0d6889;
+            background-color: #fff;
+            border-radius: 1px;
+        }
+
         /* Estilos específicos para la impresión */
         @media print {
             * {
@@ -68,125 +92,257 @@
             .oculto-impresion {
                 display: none;
             }
+
         }
     </style>
 </head>
 
 <body class="py-4 text-center">
-    <div class="rounded ticket">
-        <img src="{{ asset('img/imagenes-07.png') }}" alt="Harsi Logo" width="140" height="50">
 
-        <p class="pt-2 text-center">
-
-            @if($restaurante->nombre != '')
-                <b class="pb-0 mb-0 text-uppercase" style="font-size: 1.1rem !important">{{$restaurante->nombre}}</b>
-                <br>
-            @endif
-            <span style="font-size: 0.65rem !important">- TICKET DE VENTA -</span>
-
-        <p class="px-2 text-left">
-
-            Folio: &nbsp;{{ $orden->id }}
-
-            @if($restaurante->direccion != '')
-                <br>
-                Calle:  &nbsp;<span class="text-uppercase">{{$restaurante->direccion}}</span>
-            @endif
-
-            @if($restaurante->rfc != '')
-                <br>
-                RFC:  &nbsp;<span class="text-uppercase">{{$restaurante->rfc}}</span>
-            @endif
+    <div class="d-flex justify-content-center">
+        <ul class="mb-4 nav nav-tabs oculto-impresion" id="myTab" role="tablist">
+            <li class="mx-2 nav-item">
+                <a class="nav-link active" id="external-tab" href="#external" role="tab" aria-controls="external" aria-selected="true">
+                    Ticket Externo
+                </a>
+            </li>
+            <li class="mx-2 nav-item">
+                <a class="nav-link" id="internal-tab" href="#internal" role="tab" aria-controls="internal" aria-selected="false">
+                    Ticket Interno
+                </a>
+            </li>
+        </ul>
+    </div>
 
 
-            <br><br>Mesa:  &nbsp;<span class="text-uppercase">{{ $orden->mesa }} -- {{ $orden->num_comensales }} Comensales</span>
-            <br>Turno:  &nbsp;<span class="text-uppercase">{{ $orden->turno }}</span>
+    <div class="tab-content" id="ticketTabs">
 
-            <br>Atendío &nbsp;<span class="text-uppercase">{{ $orden->mesero }}</span>
-            <br>Cobró: &nbsp;<span class="text-uppercase">{{ $orden->cajero }}</span>
+        {{-- External Ticket (Con Guia) --}}
+        <div class="tab-pane fade show active" id="external" role="tabpanel" aria-labelledby="external-tab">
+            <div class="rounded ticket">
+                <img src="{{ asset('img/imagenes-07.png') }}" alt="Harsi Logo" width="140" height="50">
 
-            @if($orden->guia != 'Ninguno')
-                <br>Guía: &nbsp;<span class="text-uppercase">{{ $orden->guia }}</span>
-            @endif
+                <p class="pt-2 text-center">
 
-            <br>Fecha: {{ $orden->created_at }}
+                    @if($restaurante->nombre != '')
+                        <b class="pb-0 mb-0 text-uppercase" style="font-size: 1.1rem !important">{{$restaurante->nombre}}</b>
+                        <br>
+                    @endif
+                    <span style="font-size: 0.65rem !important">- TICKET DE VENTA -</span>
 
-            @if($orden->mesa == 'Para llevar')
-                <br><br>Cliente: &nbsp;<span class="text-uppercase">{{ $orden->cliente }}</span>
-                <br>Dirección: &nbsp;<span class="text-uppercase">{{ $orden->direccion }}</span>
-            @endif
+                <p class="px-2 text-left">
+
+                    Folio: &nbsp;{{ $orden->id }}
+
+                    @if($restaurante->direccion != '')
+                        <br>
+                        Calle:  &nbsp;<span class="text-uppercase">{{$restaurante->direccion}}</span>
+                    @endif
+
+                    @if($restaurante->rfc != '')
+                        <br>
+                        RFC:  &nbsp;<span class="text-uppercase">{{$restaurante->rfc}}</span>
+                    @endif
 
 
-        </p>
-        <table class="mt-2 mb-3">
-            <thead>
-                <tr>
-                    <th class="cantidad">CANT</th>
-                    <th class="producto">PRODUCTO</th>
-                    <th class="precio">PRECIO</th>
-                    <th class="subtotal">IMPORTE</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($pedidos as $pedido)
-                    <tr>
-                        <td class="cantidad">{{ $pedido->cantidad }}</td>
-                        <td class="text-left producto">{{ $pedido->articulo }}</td>
-                        <td class="precio">${{ $pedido->precio_compra }}</td>
-                        <td class="subtotal">${{ $pedido->subtotal }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <p class="text-center">
+                    <br><br>Mesa:  &nbsp;<span class="text-uppercase">{{ $orden->mesa }} -- {{ $orden->num_comensales }} Comensales</span>
+                    <br>Turno:  &nbsp;<span class="text-uppercase">{{ $orden->turno }}</span>
 
-            Número de Articulos: &nbsp;{{ $pedido_count }}
-            <br>Forma de Pago: &nbsp;{{ $orden->forma_pago }}
+                    <br>Atendío &nbsp;<span class="text-uppercase">{{ $orden->mesero }}</span>
+                    <br>Cobró: &nbsp;<span class="text-uppercase">{{ $orden->cajero }}</span>
+                    <br>Fecha: {{ $orden->created_at }}
 
-            @if($orden->descuento != null)
-                <br>Importe: &nbsp;${{ $orden->conf_total }}
-                <br>Descuento: &nbsp;{{ $orden->descuento }} %
-            @endif
+                    @if($orden->mesa == 'Para llevar')
+                        <br><br>Cliente: &nbsp;<span class="text-uppercase">{{ $orden->cliente }}</span>
+                        <br>Dirección: &nbsp;<span class="text-uppercase">{{ $orden->direccion }}</span>
+                    @endif
 
-            @if($orden->guia != 'Ninguno')
-                <br>Subtotal: &nbsp;${{ $orden->total }}
-                <br>
-                Importe por Guia ({{ $orden->comision_percentage }}%):
-                &nbsp;${{ $orden->comision }}
-            @endif
 
-            @if($orden->propina != null)
-                <br>Propina: &nbsp;${{ $orden->propina }}
-            @endif
+                </p>
+                <table class="mt-2 mb-3">
+                    <thead>
+                        <tr>
+                            <th class="cantidad">CANT</th>
+                            <th class="producto">PRODUCTO</th>
+                            <th class="precio">PRECIO</th>
+                            <th class="subtotal">IMPORTE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($pedidos as $pedido)
+                            <tr>
+                                <td class="cantidad">{{ $pedido->cantidad }}</td>
+                                <td class="text-left producto">{{ $pedido->articulo }}</td>
+                                <td class="precio">${{ $pedido->precio_compra }}</td>
+                                <td class="subtotal">${{ $pedido->subtotal }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <p class="text-center">
 
-            <br><b>Total: &nbsp;${{ $orden->total2 - $orden->propina }} M.N</b>
-            <br>Pago: &nbsp;${{ $orden->pago }}
-            <br>Cambio: &nbsp;${{ $orden->cambio + $orden->propina }}
-        </p>
+                    Número de Articulos: &nbsp;{{ $pedido_count }}
+                    <br>Forma de Pago: &nbsp;{{ $orden->forma_pago }}
 
-        <p class="text-center">¡GRACIAS POR SU PREFERENCIA!
-            @if($restaurante->telefono != '')
-            <br>Telefono: {{$restaurante->telefono}}
-            @endif
-            @if($restaurante->email != '')
-            <br>Correo: {{$restaurante->email}}
-            @endif
-            @if($restaurante->facebook != '')
-            <br>Facebook: {{$restaurante->facebook}}
-            @endif
-            @if($restaurante->instagram != '')
-            <br>Instagram:{{$restaurante->instagram}}
-            @endif
-            @if($restaurante->twitter != '')
-            <br>Twitter: {{$restaurante->twitter}}
-            @endif
-            @if($restaurante->youTube != '')
-            <br>Youtube: {{$restaurante->youTube}}
-            @endif
-            @if($restaurante->linkedIn != '')
-            <br>LinkedIn: {{$restaurante->linkedIn}}
-            @endif
-        </p>
+                    @if($orden->descuento != null)
+                        <br>Importe: &nbsp;${{ $orden->conf_total }}
+                        <br>Descuento: &nbsp;{{ $orden->descuento }} %
+                    @endif
+
+                    @if($orden->propina != null)
+                        <br>Propina: &nbsp;${{ $orden->propina }}
+                    @endif
+
+                    <br><b>Total: &nbsp;${{ $orden->total2 - $orden->propina }} M.N</b>
+                    <br>Pago: &nbsp;${{ $orden->pago }}
+                    <br>Cambio: &nbsp;${{ $orden->cambio + $orden->propina }}
+                </p>
+
+                <p class="text-center">¡GRACIAS POR SU PREFERENCIA!
+                    @if($restaurante->telefono != '')
+                    <br>Telefono: {{$restaurante->telefono}}
+                    @endif
+                    @if($restaurante->email != '')
+                    <br>Correo: {{$restaurante->email}}
+                    @endif
+                    @if($restaurante->facebook != '')
+                    <br>Facebook: {{$restaurante->facebook}}
+                    @endif
+                    @if($restaurante->instagram != '')
+                    <br>Instagram:{{$restaurante->instagram}}
+                    @endif
+                    @if($restaurante->twitter != '')
+                    <br>Twitter: {{$restaurante->twitter}}
+                    @endif
+                    @if($restaurante->youTube != '')
+                    <br>Youtube: {{$restaurante->youTube}}
+                    @endif
+                    @if($restaurante->linkedIn != '')
+                    <br>LinkedIn: {{$restaurante->linkedIn}}
+                    @endif
+                </p>
+            </div>
+        </div>
+
+        {{-- Internal Ticket  (Sin Guia)  --}}
+        <div class="tab-pane fade" id="internal" role="tabpanel" aria-labelledby="internal-tab">
+            <div class="rounded ticket">
+                <img src="{{ asset('img/imagenes-07.png') }}" alt="Harsi Logo" width="140" height="50">
+
+                <p class="pt-2 text-center">
+
+                    @if($restaurante->nombre != '')
+                        <b class="pb-0 mb-0 text-uppercase" style="font-size: 1.1rem !important">{{$restaurante->nombre}}</b>
+                        <br>
+                    @endif
+                    <span style="font-size: 0.65rem !important">- TICKET DE VENTA -</span>
+
+                <p class="px-2 text-left">
+
+                    Folio: &nbsp;{{ $orden->id }}
+
+                    @if($restaurante->direccion != '')
+                        <br>
+                        Calle:  &nbsp;<span class="text-uppercase">{{$restaurante->direccion}}</span>
+                    @endif
+
+                    @if($restaurante->rfc != '')
+                        <br>
+                        RFC:  &nbsp;<span class="text-uppercase">{{$restaurante->rfc}}</span>
+                    @endif
+
+
+                    <br><br>Mesa:  &nbsp;<span class="text-uppercase">{{ $orden->mesa }} -- {{ $orden->num_comensales }} Comensales</span>
+                    <br>Turno:  &nbsp;<span class="text-uppercase">{{ $orden->turno }}</span>
+
+                    <br>Atendío &nbsp;<span class="text-uppercase">{{ $orden->mesero }}</span>
+                    <br>Cobró: &nbsp;<span class="text-uppercase">{{ $orden->cajero }}</span>
+
+                    @if($orden->guia != 'Ninguno')
+                        <br>Guía: &nbsp;<span class="text-uppercase">{{ $orden->guia }}</span>
+                    @endif
+
+                    <br>Fecha: {{ $orden->created_at }}
+
+                    @if($orden->mesa == 'Para llevar')
+                        <br><br>Cliente: &nbsp;<span class="text-uppercase">{{ $orden->cliente }}</span>
+                        <br>Dirección: &nbsp;<span class="text-uppercase">{{ $orden->direccion }}</span>
+                    @endif
+
+
+                </p>
+                <table class="mt-2 mb-3">
+                    <thead>
+                        <tr>
+                            <th class="cantidad">CANT</th>
+                            <th class="producto">PRODUCTO</th>
+                            <th class="precio">PRECIO</th>
+                            <th class="subtotal">IMPORTE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($pedidos as $pedido)
+                            <tr>
+                                <td class="cantidad">{{ $pedido->cantidad }}</td>
+                                <td class="text-left producto">{{ $pedido->articulo }}</td>
+                                <td class="precio">${{ $pedido->precio_compra }}</td>
+                                <td class="subtotal">${{ $pedido->subtotal }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <p class="text-center">
+
+                    Número de Articulos: &nbsp;{{ $pedido_count }}
+                    <br>Forma de Pago: &nbsp;{{ $orden->forma_pago }}
+
+                    @if($orden->descuento != null)
+                        <br>Importe: &nbsp;${{ $orden->conf_total }}
+                        <br>Descuento: &nbsp;{{ $orden->descuento }} %
+                    @endif
+
+                    @if($orden->guia != 'Ninguno')
+                        <br>Subtotal: &nbsp;${{ $orden->total }}
+                        <br>
+                        Importe por Guia ({{ $orden->comision_percentage }}%):
+                        &nbsp;${{ $orden->comision }}
+                    @endif
+
+                    @if($orden->propina != null)
+                        <br>Propina: &nbsp;${{ $orden->propina }}
+                    @endif
+
+                    <br><b>Total: &nbsp;${{ $orden->total2 - $orden->propina }} M.N</b>
+                    <br>Pago: &nbsp;${{ $orden->pago }}
+                    <br>Cambio: &nbsp;${{ $orden->cambio + $orden->propina }}
+                </p>
+
+                <p class="text-center">¡GRACIAS POR SU PREFERENCIA!
+                    @if($restaurante->telefono != '')
+                    <br>Telefono: {{$restaurante->telefono}}
+                    @endif
+                    @if($restaurante->email != '')
+                    <br>Correo: {{$restaurante->email}}
+                    @endif
+                    @if($restaurante->facebook != '')
+                    <br>Facebook: {{$restaurante->facebook}}
+                    @endif
+                    @if($restaurante->instagram != '')
+                    <br>Instagram:{{$restaurante->instagram}}
+                    @endif
+                    @if($restaurante->twitter != '')
+                    <br>Twitter: {{$restaurante->twitter}}
+                    @endif
+                    @if($restaurante->youTube != '')
+                    <br>Youtube: {{$restaurante->youTube}}
+                    @endif
+                    @if($restaurante->linkedIn != '')
+                    <br>LinkedIn: {{$restaurante->linkedIn}}
+                    @endif
+                </p>
+            </div>
+        </div>
+
     </div>
 
     <button class="mt-4 btn btn-success oculto-impresion" onclick="imprimir()">Imprimir</button>
@@ -195,6 +351,16 @@
         function imprimir() {
             window.print();
         }
+
+        $(document).ready(function() {
+
+            $("#external").tab('show');
+
+            $('.nav-tabs a').on('click', function(event) {
+                event.preventDefault(); // Evitar el comportamiento predeterminado
+                $(this).tab('show'); // Mostrar la pestaña correspondiente
+            });
+        });
     </script>
 </body>
 
